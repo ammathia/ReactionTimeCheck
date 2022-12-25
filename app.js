38d39304e1036b71052e3d9e4ea6ebc2293de413
Grosul Igor <grosuligor99@mail.ru>
2022-12-25 12:03:27 +0200

    1

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 "use strict"
 
+function clickReaction() {
+    document.getElementById("reaction").innerHTML = `Your reaction time is THIS`;
+}
 
+
+setTimeout(appearObject, +(1000 * Math.round((Math.random()*(8-3)+3)*100)/100));
 
 function appearObject() {
-    document.getElementById("object").display = none;
+    
+    let rep = +Math.round(Math.random()*(20-5)+5);
+    let rep1 = rep + "rem";
+    
+    document.getElementById("object").style.left = rep1 ;
+    document.getElementById("object").style.top = rep1 ;
+    document.getElementById("object").style.display = "block";
+
+}
+
+function appearObjectq() {
+    
+    
+    //document.getElementById("object").style.display = "none";
+
 }

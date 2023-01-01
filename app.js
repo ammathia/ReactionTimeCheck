e185d69824875d65c268ea7ed5852b6866064ca2
Grosul Igor <grosuligor99@mail.ru>
2023-01-01 12:59:47 +0200

    color

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
 
 window.onload = setTimeout(appearObject, +(1000 * Math.round((Math.random()*(10-5)+5)*100)/100));
 
+let start;
+let end;
+
+let colors = ["white", "black", "red", "green", "yellow", "blue"];
 
 function appearObject() {
     
@@ -10,20 +14,23 @@ function appearObject() {
     let rep1 = rep + "rem";
     let rip = +Math.round(Math.random()*(20-5)+5);
     let rip1 = rip + "rem";
+
+    let colorRandom = +Math.round(Math.random()*(5-0)+0);
     
-    let start = Date.now();
+    start = Date.now();
+    
+    document.getElementById("object").style.color = rep1 ;
     document.getElementById("object").style.left = rep1 ;
     document.getElementById("object").style.top = rip1 ;
     document.getElementById("object").style.display = "block";
 
 }
 
-function disappear() {
+function disappearObject() {
     
     document.getElementById("object").style.display = "none";
-    let end = Date.now();
-    let clickedTime = (start - end)/1000;
-    alert(clickedTime);
+    end = Date.now();
+    let clickedTime = (end - start)/1000;
     document.getElementById("reaction").innerHTML = "Your reaction time is " + clickedTime; 
     setTimeout(appearObject, +(1000 * Math.round((Math.random()*(10-5)+5)*100)/100));
 
